187897bba7a1e13c4c40bbb36ca5ca5434b384e1
snljty <snljty@gmail.com>
2022-05-14 17:38:30 +0800

    use environment variable instead of predefined macro

--- C-version/FrogJumpWalk_game.c
+++ C-version/FrogJumpWalk_game.c
@@ -3,20 +3,22 @@
 # include <string.h>
 # include <stdbool.h>
 
-/*  # define PRINT_WITH_COLOR  */
+/* # define PRINT_WITH_COLOR */
 
 # ifdef linux
-  # include <termio.h>
-  int getch();
-  # define stricmp_unity(str1, str2) strcasecmp(str1, str2)
+    # include <termio.h>
+    int getch();
 # else
-  # ifdef WIN32
-    # include <conio.h>
-    # define stricmp_unity(str1, str2) stricmp(str1, str2)
-  # endif
+    # ifdef WIN32
+        # include <conio.h>
+    # endif
 # endif
 
+
+
 # define DeleteBoard(p)    free(p); p = NULL;
+
+bool isPrintWithColor = false;
 
 void SwapChar(char *p, char *q);
 
@@ -43,316 +45,364 @@ void RunGame(char *board, unsigned int nFrogs);
 
 int main(int argc, char const *argv[])
 {
-  char *board = NULL;
-  unsigned int nFrogs = 0;
+    char *board = NULL;
+    unsigned int nFrogs = 0;
 
-  /*  print game hint  */
-  PrintHint();
-  /*  acquire the size of the board  */
-  nFrogs = AcquireSize(argc, argv);
-  /*  initialize the board  */
-  board = InitializeBoard(nFrogs);
-  /*  run the game  */
-  PrintBoard(board);
-  RunGame(board, nFrogs);
-  /*  delete the board  */
-  DeleteBoard(board);
+    /* print game hint */
+    PrintHint();
+    /* acquire the size of the board */
+    nFrogs = AcquireSize(argc, argv);
+    /* initialize the board */
+    board = InitializeBoard(nFrogs);
+    /* run the game */
+    PrintBoard(board);
+    RunGame(board, nFrogs);
+    /* delete the board */
+    DeleteBoard(board);
 
-  return 0;
+    return 0;
 }
 
 
 void SwapChar(char *p, char *q)
 {
-  char t;
-  
-  t = * p;
-  * p = * q;
-  * q = t;
+    char t;
+    
+    t = * p;
+    * p = * q;
+    * q = t;
 
-  return;
+    return;
 }
 
 void PrintHint(void)
 {
-  printf("This program is a game of frogs jumping or walking.\n");
-  printf("\n");
-  printf("The frogs '>' are heading right, and can only go right.\n");
-  printf("The frogs '<' are heading  left, and can only go  left.\n");
-  printf("'_' is a blank position.\n");
-  printf("If a frog is on the side of the blank position, \n");
-  printf("Then it can walk to the blank, \n");
-  printf("as long as it is on the correct direction.\n");
-  printf("Or if a frog is separated by another frog from the blank, \n");
-  printf("it can jump over to the blank if in the correct direction.\n");
-  printf("\n");
-  printf("Your aim is to swap the positions of all frogs '>' and '<'.\n");
-  printf("Press 'f' to walk a frog right, 'd' to jump a frog right, \n");
-  printf("'j' to walf a frog right, 'k' to jump a frog left, \n");
-  printf("and 'r' to reset the game.\n");
-  printf("Press <Enter> to exit.\n");
-  printf("\n");
+    printf("This program is a game of frogs jumping or walking.\n");
+    printf("\n");
+    printf("The frogs '>' are heading right, and can only go right.\n");
+    printf("The frogs '<' are heading    left, and can only go    left.\n");
+    printf("'_' is a blank position.\n");
+    printf("If a frog is on the side of the blank position, \n");
+    printf("Then it can walk to the blank, \n");
+    printf("as long as it is on the correct direction.\n");
+    printf("Or if a frog is separated by another frog from the blank, \n");
+    printf("it can jump over to the blank if in the correct direction.\n");
+    printf("\n");
+    printf("Your aim is to swap the positions of all frogs '>' and '<'.\n");
+    printf("Press 'f' to walk a frog right, 'd' to jump a frog right, \n");
+    printf("'j' to walf a frog right, 'k' to jump a frog left, \n");
+    printf("and 'r' to reset the game.\n");
+    printf("Press <Enter> to exit.\n");
+    printf("\n");
 }
 
 unsigned int AcquireSize(int argc, char const *argv[])
 {
-  unsigned int nFrogs = 0;
-  char c = '\0';
-  unsigned int const nFrogsDef = 3;
-  char buf[BUFSIZ + 1] = "";
+    unsigned int nFrogs = 0;
+    char c = '\0';
+    unsigned int const nFrogsDef = 3;
+    char buf[BUFSIZ + 1] = "";
 
-  for (;;)
-  {
-    if (argc > 2)
-    {
-      fprintf(stderr, "At most 1 command argument should be provided, but got %d.\n", (argc - 1));
-      fprintf(stderr, "Please use the interactive mode.\n");
-    }
-    else if (argc == 2)
-    {
-      if (! stricmp_unity(argv[1], "default"))
-      {
-        nFrogs = nFrogsDef;
-        break;
-      }
-      if (sscanf(argv[1], "%u", & nFrogs) == 1)
-      {
-        if ((int)nFrogs > 0)
-          break;
-        fprintf(stderr, "A positive number for amount of frogs is required, but got %d.\n", (int)nFrogs);
-        fprintf(stderr, "Please use the interactive mode.\n");
-      }
-      else
-        fprintf(stderr, "Unrecognizable number \"%s\", please use the interactive mode.\n", argv[1]);
-    }
-    printf("Input the number of frogs on each side.\n");
-    printf("Press <Enter> directly to use the default value: %u.\n", nFrogsDef);
     for (;;)
     {
-      fgets(buf, BUFSIZ, stdin);
-      while (strlen(buf) > 0 && buf[strlen(buf) - 1] == '\n')
-        buf[strlen(buf) - 1] = '\0';
-      if (! strcmp(buf, ""))
-      {
-        nFrogs = nFrogsDef;
+        if (argc > 2)
+        {
+            fprintf(stderr, "At most 1 command argument should be provided, but got %d.\n", (argc - 1));
+            fprintf(stderr, "Please use the interactive mode.\n");
+        }
+        else if (argc == 2)
+        {
+            if (! strcasecmp(argv[1], "default"))
+            {
+                nFrogs = nFrogsDef;
+                break;
+            }
+            if (sscanf(argv[1], "%u", & nFrogs) == 1)
+            {
+                if ((int)nFrogs > 0)
+                    break;
+                fprintf(stderr, "A positive number for amount of frogs is required, but got %d.\n", (int)nFrogs);
+                fprintf(stderr, "Please use the interactive mode.\n");
+            }
+            else
+                fprintf(stderr, "Unrecognizable number \"%s\", please use the interactive mode.\n", argv[1]);
+        }
+        printf("Input the number of frogs on each side.\n");
+        printf("Press <Enter> directly to use the default value: %u.\n", nFrogsDef);
+        for (;;)
+        {
+            fgets(buf, BUFSIZ, stdin);
+            if (strlen(buf) > 0 && buf[strlen(buf) - 1] == '\n')
+            {
+                buf[strlen(buf) - 1] = '\0';
+            }
+            if (! strcmp(buf, ""))
+            {
+                nFrogs = nFrogsDef;
+                break;
+            }
+            if (sscanf(buf, "%u", & nFrogs) == 1)
+            {
+                if (nFrogs)
+                {
+                    break;
+                }
+                fprintf(stderr, "There should be at least one frog, please input again.\n");
+            }
+            else
+                fprintf(stderr, "Unrecognizable number \"%s\", please input again.\n", buf);
+        }    
         break;
-      }
-      if (sscanf(buf, "%u", & nFrogs) == 1)
-      {
-        if (nFrogs)
-          break;
-        fprintf(stderr, "There should be at least one frog, please input again.\n");
-      }
-      else
-        fprintf(stderr, "Unrecognizable number \"%s\", please input again.\n", buf);
-    }  
-    break;
-  }
-  printf("\n");
-  printf("\n");
+    }
+    printf("\n");
+    printf("\n");
 
-  return nFrogs;
+    return nFrogs;
 }
 
 char *InitializeBoard(unsigned int nFrogs)
 {
-  char *board = NULL;
-  unsigned int i = 0;
+    char *board = NULL;
+    unsigned int i = 0;
 
-  board = (char *)malloc((2 * nFrogs + 2) * sizeof(char));
-  board[2 * nFrogs + 1] = '\0';
-  for (i = 0; i < nFrogs; i ++)
-    board[i] = '>';
-  board[nFrogs] = '_';
-  for (i = nFrogs + 1; i <= 2 * nFrogs; i ++)
-    board[i] = '<';
-  system("");
+    board = (char *)malloc((2 * nFrogs + 2) * sizeof(char));
+    board[2 * nFrogs + 1] = '\0';
+    for (i = 0; i < nFrogs; i ++)
+    {
+        board[i] = '>';
+    }
+    board[nFrogs] = '_';
+    for (i = nFrogs + 1; i <= 2 * nFrogs; i ++)
+    {
+        board[i] = '<';
+    }
+    if (getenv("PRINT_WITH_COLOR"))
+    {
+        system("");
+        isPrintWithColor = true;
+    }
+    else
+    {
+        isPrintWithColor = false;
+    }
 
-  return board;
+    return board;
 }
 
 void ResetBoard(char *board, unsigned int nFrogs, unsigned int *posBlankPointer)
 {
-  unsigned int i = 0;
+    unsigned int i = 0;
 
-  for (i = 0; i < nFrogs; i ++)
-    board[i] = '>';
-  board[nFrogs] = '_';
-  for (i = nFrogs + 1; i <= 2 * nFrogs; i ++)
-    board[i] = '<';
-  * posBlankPointer = nFrogs;
+    for (i = 0; i < nFrogs; i ++)
+    {
+        board[i] = '>';
+    }
+    board[nFrogs] = '_';
+    for (i = nFrogs + 1; i <= 2 * nFrogs; i ++)
+    {
+        board[i] = '<';
+    }
+    * posBlankPointer = nFrogs;
 
-  return;
+    return;
 }
 
 bool IsFinished(char *board, unsigned int nFrogs)
 {
-  unsigned int i = 0;
+    unsigned int i = 0;
 
-  for (i = 0; i < nFrogs; i ++)
-    if (board[i] != '<')
-      return false;
-  if (board[nFrogs] != '_')
-    return false;
-  return true;
+    for (i = 0; i < nFrogs; i ++)
+    {
+        if (board[i] != '<')
+        {
+            return false;
+        }
+    }
+    if (board[nFrogs] != '_')
+    {
+        return false;
+    }
+    return true;
 }
 
 void PauseGame(void)
 {
-  char c;
+    char c;
 
-  while ((c = getchar()) != '\n' && c != EOF)
-    ;
+    while ((c = getchar()) != '\n' && c != EOF)
+    {
+        ;
+    }
 
-  return;
+    return;
 }
 
 void PrintBoard(char *board)
 {
-  unsigned int i = 0;
-  unsigned int posBlank = 0;
-  unsigned int j = 0;
+    unsigned int i = 0;
+    unsigned int posBlank = 0;
+    unsigned int j = 0;
 
-  printf("%c", '\r');
-  while (board[i])
-  {
-    if (board[i] == '_')
-      posBlank = i;
-    # ifdef PRINT_WITH_COLOR
-      if (board[i] == '>')
-        printf("\e[31m");
-      else if (board[i] == '<')
-        printf("\e[34m");
-      else
-        printf("\e[32m");
-    # endif
-    printf("%c", board[i]);
-    # ifdef PRINT_WITH_COLOR
-      printf("\e[0m");
-    # endif
-    ++ i;
-  }
-  for (j = i; j > posBlank; -- j)
-    printf("%c", '\b');
+    printf("%c", '\r');
+    while (board[i])
+    {
+        if (board[i] == '_')
+        {
+            posBlank = i;
+        }
+        if (isPrintWithColor)
+        {
+            if (board[i] == '>')
+            {
+                printf("\e[31m");
+            }
+            else if (board[i] == '<')
+            {
+                printf("\e[34m");
+            }
+            else
+            {
+                printf("\e[32m");
+            }
+        }
+        printf("%c", board[i]);
+        if (isPrintWithColor)
+        {
+            printf("\e[0m");
+        }
+        ++ i;
+    }
+    for (j = i; j > posBlank; -- j)
+    {
+        printf("%c", '\b');
+    }
 
-  return;
+    return;
 }
 
 void PrintEndGame(void)
 {
-  printf("\n");
-  printf("Game ended. Press <Enter> to exit.\n");
-  PauseGame();
+    printf("\n");
+    printf("Game ended. Press <Enter> to exit.\n");
+    PauseGame();
 
-  return;
+    return;
 }
 
 void PrintFinishGame(void)
 {
-  printf("\n");
-  printf("Congratulations! You finished the game successfully!\n");
-  printf("Press <Enter> to exit.\n");
-  PauseGame();
+    printf("\n");
+    printf("Congratulations! You finished the game successfully!\n");
+    printf("Press <Enter> to exit.\n");
+    PauseGame();
 
-  return;
+    return;
 }
 
 void RunGame(char *board, unsigned int nFrogs)
 {
-  char c = '\0';
-  unsigned int posBlank = nFrogs;
-  bool ichanged = false;
+    char c = '\0';
+    unsigned int posBlank = nFrogs;
+    bool ichanged = false;
 
-  while (true)
-  {
-    c = getch();
-    if ((c == '\r') || (c == EOF))
-      break;
-
-    ichanged = false;
-    switch (c)
+    for (;;)
     {
-    case 'f': /*  walk right  */
-      if ((posBlank == 0) || (board[posBlank - 1] != '>'))
-      {
-        printf("\a");
-        break;
-      }
-      ichanged = true;
-      SwapChar(board + posBlank - 1, board + posBlank);
-      posBlank --;
-      break;
-    case 'j': /*  walk left  */
-      if ((posBlank == 2 * nFrogs) || (board[posBlank + 1] != '<'))
-      {
-        printf("\a");
-        break;
-      }
-      ichanged = true;
-      SwapChar(board + posBlank + 1, board + posBlank);
-      posBlank ++;
-      break;
-    case 'd': /*  jump right  */
-      if ((posBlank <= 1) || (board[posBlank - 2] != '>'))
-      {
-        printf("\a");
-        break;
-      }
-      ichanged = true;
-      SwapChar(board + posBlank - 2, board + posBlank);
-      posBlank -= 2;
-      break;
-    case 'k': /*  walk left  */
-      if ((posBlank >= 2 * nFrogs - 1) || (board[posBlank + 2] != '<'))
-      {
-        printf("\a");
-        break;
-      }
-      ichanged = true;
-      SwapChar(board + posBlank + 2, board + posBlank);
-      posBlank += 2;
-      break;
-    case 'r': /*  reset  */
-      ichanged = true;
-      ResetBoard(board, nFrogs, & posBlank);
-      break;
-    default:
-      printf("\a");
-      break;
-    }
-    if (ichanged)
-    {
-      PrintBoard(board);
-      if (IsFinished(board, nFrogs))
-      {
-        /*  game finished  */
-        PrintFinishGame();
-        return;
-      }
-    }
-  }
+        c = getch();
+        if ((c == '\r') || (c == EOF))
+        {
+            break;
+        }
 
-  PrintEndGame();
-  return;
+        ichanged = false;
+        switch (c)
+        {
+        case 'f': /* walk right */
+            if ((posBlank == 0) || (board[posBlank - 1] != '>'))
+            {
+                printf("\a");
+                break;
+            }
+            ichanged = true;
+            SwapChar(board + posBlank - 1, board + posBlank);
+            posBlank --;
+            break;
+        case 'j': /* walk left */
+            if ((posBlank == 2 * nFrogs) || (board[posBlank + 1] != '<'))
+            {
+                printf("\a");
+                break;
+            }
+            ichanged = true;
+            SwapChar(board + posBlank + 1, board + posBlank);
+            posBlank ++;
+            break;
+        case 'd': /* jump right */
+            if ((posBlank <= 1) || (board[posBlank - 2] != '>'))
+            {
+                printf("\a");
+                break;
+            }
+            ichanged = true;
+            SwapChar(board + posBlank - 2, board + posBlank);
+            posBlank -= 2;
+            break;
+        case 'k': /* walk left */
+            if ((posBlank >= 2 * nFrogs - 1) || (board[posBlank + 2] != '<'))
+            {
+                printf("\a");
+                break;
+            }
+            ichanged = true;
+            SwapChar(board + posBlank + 2, board + posBlank);
+            posBlank += 2;
+            break;
+        case 'r': /* reset */
+            ichanged = true;
+            ResetBoard(board, nFrogs, & posBlank);
+            break;
+        default:
+            printf("\a");
+            break;
+        }
+        if (ichanged)
+        {
+            PrintBoard(board);
+            if (IsFinished(board, nFrogs))
+            {
+                /* game finished */
+                PrintFinishGame();
+                return;
+            }
+        }
+    }
+
+    PrintEndGame();
+    return;
 }
 
 # ifdef linux
-  int getch()
-  {
-    struct termios tm, tm_old;
-    int fd = 0, ch;
+    int getch()
+    {
+        struct termios tm, tm_old;
+        int fd = 0, ch;
 
-    if (tcgetattr(fd, & tm) < 0)
-      return -1;
-    tm_old = tm;
-    cfmakeraw(& tm);
-    if (tcsetattr(fd, TCSANOW, & tm) < 0)
-      return -1;
-    ch = getchar();
-    if (tcsetattr(fd, TCSANOW, &tm_old) < 0)
-      return -1;
-  
-    return ch;
-  }
+        if (tcgetattr(fd, & tm) < 0)
+        {
+            return -1;
+        }
+        tm_old = tm;
+        cfmakeraw(& tm);
+        if (tcsetattr(fd, TCSANOW, & tm) < 0)
+        {
+            return -1;
+        }
+        ch = getchar();
+        if (tcsetattr(fd, TCSANOW, &tm_old) < 0)
+        {
+            return -1;
+        }
+    
+        return ch;
+    }
 # endif
 
--- C-version/FrogJumpWalk_solve.c
+++ C-version/FrogJumpWalk_solve.c
@@ -3,136 +3,195 @@
 # include <string.h>
 # include <stdbool.h>
 
-/*  This program show solutions of the game frog_walk_or_jump.  */
+/* This program show solutions of the game frog_walk_or_jump. */
 
-/*  # define PRINT_WITH_COLOR  */
-
-# define COLOR_RED   0x01
+# define COLOR_RED     0x01
 # define COLOR_GREEN 0x02
-# define COLOR_BLUE  0x04
+# define COLOR_BLUE    0x04
 
 char const jump[] = {'d', 'k'};
 char const walk[] = {'f', 'j'};
 
-void Print_symbol(char const *operation, int index);
+bool isPrintWithColor = false;
+
+void PrintSymbol(char const *operation, int index);
 
 int main(int argc, char const *argv[])
 {
-  unsigned int nFrogs = 0;
-  unsigned int i = 0, j = 0;
-  char pauser = '\0';
-  unsigned int const nFrogsDef = 3;
-  char buf[BUFSIZ + 1] = "";
-  bool isInteractiveMode = true;
+    unsigned int nFrogs = 0;
+    unsigned int i = 0, j = 0;
+    char pauser = '\0';
+    unsigned int const nFrogsDef = 3;
+    char buf[BUFSIZ + 1] = "";
+    bool isInteractiveMode = true;
 
-  for (;;)
-  {
-    if (argc > 2)
-    {
-      fprintf(stderr, "At most 1 command argument should be provided, but got %d.\n", (argc - 1));
-      fprintf(stderr, "Please use the interactive mode.\n");
-    }
-    else if (argc == 2)
-    {
-      if (! strcasecmp(argv[1], "default"))
-      {
-        nFrogs = nFrogsDef;
-        isInteractiveMode = false;
-        break;
-      }
-      if (sscanf(argv[1], "%u", & nFrogs) == 1)
-      {
-        if ((int)nFrogs > 0)
-        {
-          isInteractiveMode = false;
-          break;
-        }
-        fprintf(stderr, "A positive number for amount of frogs is required, but got %d.\n", (int)nFrogs);
-        fprintf(stderr, "Please use the interactive mode.\n");
-      }
-      else
-        fprintf(stderr, "Unrecognizable number \"%s\", please use the interactive mode.\n", argv[1]);
-    }
-    printf("Input the number of frogs on each side.\n");
-    printf("Press <Enter> directly to use the default value: %u.\n", nFrogsDef);
     for (;;)
     {
-      fgets(buf, BUFSIZ, stdin);
-      while (strlen(buf) > 0 && buf[strlen(buf) - 1] == '\n')
-        buf[strlen(buf) - 1] = '\0';
-      if (! strcmp(buf, ""))
-      {
-        nFrogs = nFrogsDef;
+        if (argc > 2)
+        {
+            fprintf(stderr, "At most 1 command argument should be provided, but got %d.\n", (argc - 1));
+            fprintf(stderr, "Please use the interactive mode.\n");
+        }
+        else if (argc == 2)
+        {
+            if (! strcasecmp(argv[1], "default"))
+            {
+                nFrogs = nFrogsDef;
+                isInteractiveMode = false;
+                break;
+            }
+            if (sscanf(argv[1], "%u", & nFrogs) == 1)
+            {
+                if ((int)nFrogs > 0)
+                {
+                    isInteractiveMode = false;
+                    break;
+                }
+                fprintf(stderr, "A positive number for amount of frogs is required, but got %d.\n", (int)nFrogs);
+                fprintf(stderr, "Please use the interactive mode.\n");
+            }
+            else
+            {
+                fprintf(stderr, "Unrecognizable number \"%s\", please use the interactive mode.\n", argv[1]);
+            }
+        }
+        printf("Input the number of frogs on each side.\n");
+        printf("Press <Enter> directly to use the default value: %u.\n", nFrogsDef);
+        for (;;)
+        {
+            fgets(buf, BUFSIZ, stdin);
+            if (strlen(buf) > 0 && buf[strlen(buf) - 1] == '\n')
+            {
+                buf[strlen(buf) - 1] = '\0';
+            }
+            if (! strcmp(buf, ""))
+            {
+                nFrogs = nFrogsDef;
+                break;
+            }
+            if (sscanf(buf, "%u", & nFrogs) == 1)
+            {
+                break;
+            }
+            fprintf(stderr, "Unrecognizable number \"%s\", please input again.\n", buf);
+        }    
         break;
-      }
-      if (sscanf(buf, "%u", & nFrogs) == 1)
-        break;
-      fprintf(stderr, "Unrecognizable number \"%s\", please input again.\n", buf);
-    }  
-    break;
-  }
-  
-  printf("\n");
-  printf("f/d for left side and j/k for right side.\n");
-  printf("d/k for jumping and f/j for walking.\n");
-  printf("\n");
-  system("");
+    }
+    
+    printf("\n");
+    printf("f/d for left side and j/k for right side.\n");
+    printf("d/k for jumping and f/j for walking.\n");
+    printf("\n");
 
-  for (i = 1; i <= nFrogs; i ++)
-    for (j = 1; j <= i; j ++)
-      if (j == i)
-        Print_symbol(walk, ! (i % 2));
-      else
-        Print_symbol(jump, ! (i % 2));
-  for (j = 1; j <= nFrogs; j ++)
-    Print_symbol(jump, nFrogs % 2);
-  for (i = nFrogs; i >= 1; i --)
-    for (j = i; j >= 1; j --)
-      if (j == i)
-        Print_symbol(walk, ! (i % 2));
-      else
-        Print_symbol(jump, ! (i % 2));
-  printf("\n");
+    if (getenv("PRINT_WITH_COLOR"))
+    {
+        isPrintWithColor = true;
+        system("");
+    }
+    else
+    {
+        isPrintWithColor = false;
+    }
 
-  for (i = 1; i <= nFrogs; i ++)
-    for (j = 1; j <= i; j ++)
-      if (j == i)
-        Print_symbol(walk, i % 2);
-      else
-        Print_symbol(jump, i % 2);
-  for (j = 1; j <= nFrogs; j ++)
-    Print_symbol(jump, ! (nFrogs % 2));
-  for (i = nFrogs; i >= 1; i --)
-    for (j = i; j >= 1; j --)
-      if (j == i)
-        Print_symbol(walk, i % 2);
-      else
-        Print_symbol(jump, i % 2);
-  printf("\n");
+    for (i = 1; i <= nFrogs; i ++)
+    {
+        for (j = 1; j <= i; j ++)
+        {
+            if (j == i)
+            {
+                PrintSymbol(walk, ! (i % 2));
+            }
+            else
+            {
+                PrintSymbol(jump, ! (i % 2));
+            }
+        }
+    }
+    for (j = 1; j <= nFrogs; j ++)
+    {
+        PrintSymbol(jump, nFrogs % 2);
+    }
+    for (i = nFrogs; i >= 1; i --)
+    {
+        for (j = i; j >= 1; j --)
+        {
+            if (j == i)
+            {
+                PrintSymbol(walk, ! (i % 2));
+            }
+            else
+            {
+                PrintSymbol(jump, ! (i % 2));
+            }
+        }
+    }
+    printf("\n");
 
-  if (isInteractiveMode)
-  {
-    printf("\nPress <Enter> to exit...\n");
-    while ((pauser = getchar()) != '\n' && pauser != EOF)
-      ;
-  }
+    for (i = 1; i <= nFrogs; i ++)
+    {
+        for (j = 1; j <= i; j ++)
+        {
+            if (j == i)
+            {
+                PrintSymbol(walk, i % 2);
+            }
+            else
+            {
+                PrintSymbol(jump, i % 2);
+            }
+        }
+    }
+    for (j = 1; j <= nFrogs; j ++)
+    {
+        PrintSymbol(jump, ! (nFrogs % 2));
+    }
+    for (i = nFrogs; i >= 1; i --)
+    {
+        for (j = i; j >= 1; j --)
+        {
+            if (j == i)
+            {
+                PrintSymbol(walk, i % 2);
+            }
+            else
+            {
+                PrintSymbol(jump, i % 2);
+            }
+        }
+    }
+    printf("\n");
 
-  return 0;
+    if (isInteractiveMode)
+    {
+        printf("\nPress <Enter> to exit...\n");
+        while ((pauser = getchar()) != '\n' && pauser != EOF)
+        {
+            ;
+        }
+    }
+
+    return 0;
 }
 
-void Print_symbol(char const *operation, int index)
+void PrintSymbol(char const *operation, int index)
 {
-  # ifdef PRINT_WITH_COLOR
-  if (operation == walk)
-    printf("\e[3%dm", ! index ? COLOR_RED : COLOR_BLUE);
-  else if (operation == jump)
-    printf("\e[3%d;47m", ! index ? COLOR_RED : COLOR_BLUE);
-  # endif
-  printf("%c", operation[index]);
-  # ifdef PRINT_WITH_COLOR
-  printf("\e[0m");
-  # endif
+    if (isPrintWithColor)
+    {
+        if (operation == walk)
+        {
+            printf("\e[3%dm", ! index ? COLOR_RED : COLOR_BLUE);
+        }
+        else if (operation == jump)
+        {
+            printf("\e[3%d;47m", ! index ? COLOR_RED : COLOR_BLUE);
+        }
+    }
+    printf("%c", operation[index]);
+    if (isPrintWithColor)
+    {
+        printf("\e[0m");
+    }
 
-  return;
+    return;
 }
 
